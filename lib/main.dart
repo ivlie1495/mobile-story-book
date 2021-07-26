add43682a2bd5427f9fe9b715993407d1b7a31a5
ivlie7 <ivan.lie@mbiz.co.id>
2021-07-26 13:56:30 +0700

    init story book

--- lib/main.dart
+++ lib/main.dart
@@ -8,70 +8,43 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  final String title;
-
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Storybook(
-        children: [
-          Story(
-            name: 'Flat button',
-            padding: EdgeInsets.all(5),
-            background: Colors.red,
-            builder: (_, k) => MaterialButton(
-              onPressed: k.boolean(label: 'Enabled', initial: true) ? () {} : null,
-              child: Text(k.text(label: 'Text', initial: 'Flat button')),
+    return Storybook(
+      children: [
+        Story(
+          name: 'Flat button',
+          padding: EdgeInsets.all(5),
+          background: Colors.red,
+          builder: (_, k) => MaterialButton(
+            onPressed: k.boolean(label: 'Enabled', initial: true) ? () {} : null,
+            child: Text(k.text(label: 'Text', initial: 'Flat button')),
+          ),
+        ),
+        Story(
+          name: 'Raised button',
+          builder: (_, k) => RaisedButton(
+            onPressed: k.boolean(label: 'Enabled', initial: true) ? () {} : null,
+            color: k.options(
+              label: 'Color',
+              initial: Colors.deepOrange,
+              options: const [
+                Option('Red', Colors.deepOrange),
+                Option('Green', Colors.teal),
+              ],
+            ),
+            textColor: Colors.white,
+            child: Text(k.text(label: 'Text', initial: 'Raised button')),
+          ),
+        ),
+        Story.simple(
+          name: 'Input field',
+          child: const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Input field',
             ),
           ),
-          Story(
-            name: 'Raised button',
-            builder: (_, k) => RaisedButton(
-              onPressed: k.boolean(label: 'Enabled', initial: true) ? () {} : null,
-              color: k.options(
-                label: 'Color',
-                initial: Colors.deepOrange,
-                options: const [
-                  Option('Red', Colors.deepOrange),
-                  Option('Green', Colors.teal),
-                ],
-              ),
-              textColor: Colors.white,
-              child: Text(k.text(label: 'Text', initial: 'Raised button')),
-            ),
-          ),
-          Story.simple(
-            name: 'Input field',
-            child: const TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Input field',
-              ),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
